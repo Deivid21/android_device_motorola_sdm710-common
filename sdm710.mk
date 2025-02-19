00b998e76e65158833375320cd8532a45f36ee09
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-19 02:44:05 +0200

    sdm710-common: switch to common QCOM AIDL bootctrl HAL
    Change-Id: I504915adb16876a04da43c3a3f06e48a6a2a4ee7

--- sdm710.mk
+++ sdm710.mk
@@ -103,9 +103,8 @@ PRODUCT_COPY_FILES += \
 
 # Bootctrl
 PRODUCT_PACKAGES += \
-    android.hardware.boot@1.1-impl-qti \
-    android.hardware.boot@1.1-impl-qti.recovery \
-    android.hardware.boot@1.1-service
+    android.hardware.boot-service.qti \
+    android.hardware.boot-service.qti.recovery
 
 $(call soong_config_set,QTI_GPT_UTILS,USE_BSG_FRAMEWORK,false)
 
